d5c8a243cfb009421a1564fee993b52e46aab237
alt012ucsd <alt012@ucsd.edu>
2024-04-22 18:05:21 -0700

    update

--- components/eventslayout.jsx
+++ components/eventslayout.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import styles from "./eventslayout.module.css";
 
 export default function Events() {
     return (
-        <div>Events</div>
-    )
+        <div className={styles.events}>
+            <div className={styles.splitleft}> 
+                <div className={styles.centered}>
+                <h1> This Week's GBM</h1>
+                </div>   
+                
+            </div>
+
+            <div className={styles.splitright}>
+                <div className={styles.centered}>
+                    <h1>More Events:</h1>
+                    <button type={styles.button}>Upcoming</button>
+                    <button type={styles.button}>General</button>
+                    <button type={styles.button}>Star</button>
+                    <button type={styles.button}>Core</button>
+                </div>
+                <div className={styles.container}>
+                    <div className={styles.square}>
+                        <p>Box 1</p>
+                    </div>
+                    <div className={styles.square}>
+                        <p>Box 2</p>
+                    </div>
+                    <div className={styles.square}>
+                        <p>Box 3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
