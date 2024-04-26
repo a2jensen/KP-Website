095d5cd4b4f30b807214c1258a2dabb694013d2e
a2jensen <a2jensen@ucsd.edu>
2024-04-25 17:18:27 -0700

    Finished initial title page

---
+++ components/eventscard/eventscard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from ''
+
+// google calendar api 
+
+
+export default function EventsCard() {
+    return (
+        <div></div>
+    )
+}
--- components/title/title.jsx
+++ components/title/title.jsx
@@ -3,6 +3,21 @@ import styles from './title.module.css';
 
 export default function Title() {
     return (
-        <div className={styles.title}>Title</div>
+        <main className={styles.row}>
+            
+            <div className='col-5 col-s-4'>
+                <div className={styles.picture}> PICTURE DIV </div>
+            </div>
+            <div className='col-7 col-s-9'>
+                <div className={styles.header}>
+                    <h1> we believe <strong>everyone</strong> <br>
+                    </br> has a place in Kaibigang <br></br>
+                    Pilipino (KP) !
+                    </h1>
+                    <p>KP promotes acess to <strong>culture, connection,</strong>
+                    and <strong>academic retention.</strong></p>
+                </div>
+            </div>
+        </main>
     )
 }
