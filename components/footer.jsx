3717ab675e20187278124b28bf9706c87d716ffe
arapada <abirapadaa@gmail.com>
2024-04-27 21:41:38 -0700

    pull from main

---
+++ components/footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="footer-container">
+                <div className="footer-pages">
+                    <a href="home.html">Return Home</a>
+                    <a href="newsletter.html">Newsletter</a>
+                    <a href="events.html">Events</a>
+                    <a href="conmmunity.html">Community</a>
+                    <a href="contact.html">Contact</a>
+                </div>
+                <h2>kaibigan pilipino</h2>
+                <div className="footer-socials">
+                    <p>est. 1987</p>
+                    <a href="https://www.instagram.com/ucsdkp/" alt="Instagram">ig</a>
+                    <a href="https://www.facebook.com/groups/ucsdkp" alt="Facebook">fb</a>
+                    <a href="https://us14.campaign-archive.com/home/?u=b56c285dc44f6d15896d9bcfe&id=c8de67a4f2" alt="Newsletter">news</a>
+                    <p>pilipino star</p>
+                </div>
+            </div>
+        </div>
+    )
+}
