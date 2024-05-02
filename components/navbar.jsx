5cf199016ced26c67e2c69f0724ceed0dace797f
Jeffery Tung <jtung0705@gmail.com>
2024-05-01 20:57:52 -0700

    Update navbar.jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,18 +4,9 @@ import styles from './navbar.module.css'
 export default function Navbar() {
     return ( 
 
-       <div className = {styles.navBarShape}>
-       <div ></div>     
-            <ul className = {styles.list}>
-                <li className = {styles.listElementKP}><a href="">kabigang pillipino</a></li>
-                              
-                <li className = {styles.listElementHome}><a href="">homse</a></li>
-                <li className = {styles.listElementAbout}><a href="">about</a></li>
-                <li className = {styles.listElementEvents}><a href="">events</a></li>
-                <li className = {styles.listElementJoin}><a href="">join</a></li>
-            </ul>
-            {/* <div>HI<div>HELLOs</div></div> */}
-            </div>
+       <div className = {styles.navBarShape}>     
+               <div>Navbars</div>
+               </div>
 
     )
 }
