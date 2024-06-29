0ce0635cbd53b54aec179407da48945bd59baff3
angelojensen <a2jensen@ucsd.edu>
2024-06-29 10:14:43 -0700

    Finished about 3/4 of the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,17 @@ import Carousel from "../components/homePage/carousel/carousel/carousel";
 import Summary from "../components/homePage/carousel/summary/summary";
 import EventsLayout from "../components/homePage/carousel/eventslayout/eventslayout";
 import Footer from "../components/homePage/carousel/footer/footer";
+import Nav from "../components/nav/nav"
 
 
 export default async function Home() {
   return (
     <main className={styles.main}>
-     <Title />
-     <Carousel />
-     <Summary />
-     <EventsLayout />
-     <Footer />
+      <Nav />
+      <Title />
+      <Carousel />
+      <Summary />
+      <EventsLayout />
     </main>
   );
 }
--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -4,14 +4,14 @@ import styles from './navbar.module.css'
 export default function Navbar() {
     return ( 
 
-        <div className={styles.navBarShape}>
+        <div className={styles.container}>
              <ul className = {styles.list}>
                 {/* <li className = {styles.listElementKP}><a href="">Kaibigang Pilipino</a></li> */}
-                <li className = {styles.listElementHome}><a href="">home</a></li>
-                <li className = {styles.listElementAbout}><a href="">about</a></li>
-                <li className = {styles.listElementEvents}><a href="">events</a></li>
+                <li className = {styles.listElement}><a href="">home</a></li>
+                <li className = {styles.listElement}><a href="">about</a></li>
+                <li className = {styles.listElement}><a href="">events</a></li>
                 <li className = {styles.listElementKP}><a href="">kaibigang pilipino</a></li>
-                <li className = {styles.listElementJoin}><a href="">join</a></li>
+                <li className = {styles.listElement}><a href="">join</a></li>
              </ul>
              {}
         </div>
