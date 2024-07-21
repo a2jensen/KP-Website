15137df14749cdba863485290c090f68269af67f
ribaltazar <ribaltazar@ucsd.edu>
2024-07-21 01:51:30 -0700

    update 1
    still need to upload icons/ fix fonts/ etc.

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -5,17 +5,17 @@ export default function Footer() {
     return (
         <div className={styles.footer}>
             <ul className={styles.toplist}>    
-                <li className={styles.titles}><a href="">Return Home</a></li>
-                <li className={styles.titles}><a href="">Newsletter</a></li>
+                <li className={styles.titles}><a href="">Home</a></li>
+                <li className={styles.titles}><a href="">About</a></li>
                 <li className={styles.titles}><a href="">Events</a></li>
-                <li className={styles.titles}><a href="">Community</a></li>
                 <li className={styles.titles}><a href="">Contact</a></li>
             </ul>
             <h2 className={styles.kp}>kaibigang pilipino</h2>
             <ul className={styles.bottomlist}>
-                <li className={styles.bottomTitles}><a href="">est. 1987</a></li>
-                <li className={styles.bottomTitles}><a href="">IG TIKTOK LOGOS</a></li>
-                <li className={styles.bottomTitles}><a href="">KP LOGO</a></li>
+                <li className={styles.est1987}><a href="">est. 1987</a></li>
+                <li className={styles.igLogo}> <a href="">IG</a></li>
+                <li className={styles.tiktokLogo}> <a href=""></a></li>
+                <li className={styles.kpLogo}><a href="">KP LOGO</a></li>
             </ul>
         </div>
     )
--- components/homePage/carousel/eventslayout/eventslayout.jsx
+++ components/homePage/carousel/eventslayout/eventslayout.jsx
@@ -6,9 +6,12 @@ import EventsCard from '../../../eventscard/eventscard.jsx';
 export default function Events() {
     return (
         <div className={styles.events}>
-            <h1 className={styles.title}>UPCOMING EVENTS</h1>
-            <EventsCard />
-            <p className={styles.more}><a>see more events ARROW</a></p>
+            <h1 className={styles.uEvents}>UPCOMING EVENTS</h1>
+            <h1 className={styles.eventsCard}><EventsCard /></h1>
+            <div className={styles.seeMoreArrow}>
+            <p className={styles.seemore}><a href="">see more events</a></p>
+            <p className={styles.arrow}><a href="">arrow</a></p>
+            </div>
         </div>
     );
 }
