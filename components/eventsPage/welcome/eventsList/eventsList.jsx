05f97f706825b8008d6fa25680a9c85844e91544
angelojensen <a2jensen@ucsd.edu>
2024-08-10 12:23:49 -0700

    refactored events page components to use API

--- components/eventsPage/welcome/eventsList/eventsList.jsx
+++ components/eventsPage/welcome/eventsList/eventsList.jsx
@@ -12,18 +12,22 @@ export default function eventsList() {
         // Define an asynchronous function to fetch data from the API
         const fetchData = async () => {
         // Fetch data from the API
-        const res = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=ep7384Gd1DNmMyiw3sYfXSAaCljmJkBbW2bRSVdsGA9mCe4QUhKJgxhzu_wY9Ss2ViNtf_03LS7yN9FeOophFWyBSmp1e9QGm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOX0nNis3yyEipsJvvBf2fD4NgHpAmDs_b7dsj8psk4MWBosyKEb9YX8WEDfrWfjcgiLCCLCT9bkhRTcYnRq_I2YvyyEYnx8Q9z9Jw9Md8uu&lib=MCNh0E4qRm-0bTPTw2U6zBk4oyDv52cB3"); // Truncated URL for readability
+        const res = await fetch("/api/eventsAPI"); // Truncated URL for readability
         // Parse the JSON response
         const result = await res.json();
         // Update the 'data' state with the fetched data
-        setData(result.data);
+        setData(result.data.data);
         };
 
         // Call the fetchData function
         fetchData();
     }, []); // Empty dependency array ensures this effect runs only once when the component mounts
 
-    const listEvents = data.slice(0,12)
+    const listEvents = Array.isArray(data) 
+        ? data
+            .filter(event => {
+                return event.board && event.title && event.date && event.time && event.location && event.description
+            }).slice(0,99) : [];
 
     return (
         <main class={styles.container}>
