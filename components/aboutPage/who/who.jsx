36ae9c4d0f0326390293dcfe017dc4af54133ed9
Zander Vilaysane <zandervilaysane@gmail.com>
2024-09-06 03:06:04 -0700

    who are we background and media queries done (needs testing)

--- components/aboutPage/who/who.jsx
+++ components/aboutPage/who/who.jsx
@@ -9,7 +9,15 @@ import CardFlip3 from '../../whoCards/flip3.jsx';
 
 export default function who() {
   return (
-    <div className={styles.group}>          
+    <div className={styles.group}>  
+      {/* include stickers within .group nest */}
+      <img src={"/star.svg"} alt={"star sticker"} className={styles.star1} />
+      <img src={"/star.svg"} alt={"star sticker"} className={styles.star2} />
+      <img src={"/star.svg"} alt={"star sticker"} className={styles.star3} />
+      <img src={"/star.svg"} alt={"star sticker"} className={styles.star4} />
+      <img src={"/world1.svg"} alt={"world sticker"} className={styles.world1} />
+      <img src={"/jeepney1.svg"} alt={"jeep sticker"} className={styles.jeep1} />
+
       <div className={styles.header}>
           <h1> <strong> <span className={styles.KPyellow}> who </span> are we? </strong> </h1>
       </div>
