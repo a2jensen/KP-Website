8202becfe1f4195ee0cecc9dd25b8614904eb1cb
fnriv <fnrivera@ucsd.edu>
2024-09-07 12:35:44 -0700

    added texture to events layout

--- components/homePage/carousel/summary/summary.jsx
+++ components/homePage/carousel/summary/summary.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import styles from './summary.module.css'
 
-
 export default function Summary() {
     return (
         <div className={styles.quotePage}>
             <div className={styles.quoteContainer}>
-                
-
-                
                 <div className={styles.quoteText}>
                     KP meets <strong>weekly</strong> for General Body Meetings (GBM) alongside 
                     events hosted by the three boards <strong>(STAR, GEN, CORE) </strong> 
@@ -16,11 +12,8 @@ export default function Summary() {
                     <br />
                     <br />
                     Come to KP the way you want, whenever you want!
-
                 </div>
                 <div className={styles.quoteStar}></div>
-                
-               
             </div>
         </div>
     )
