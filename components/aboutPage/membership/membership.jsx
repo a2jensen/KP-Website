f18692ebc7472c8ea5ef663e64012a28bcb9fa7f
fnriv <fnrivera@ucsd.edu>
2024-09-19 21:13:44 -0700

    fixed membership font and updated description

--- components/aboutPage/membership/membership.jsx
+++ components/aboutPage/membership/membership.jsx
@@ -9,12 +9,23 @@ export default function membership() {
     <div>
       <div className={styles.container}>
         <h3 className={styles.member}>Membership</h3>
-        <p className={styles.info}>KP Membership offers perks, including 
+        <p className={styles.info}>
+        KP Membership offers perks, including 
         <strong className={styles.bold}> priority access </strong> to intramural sports teams, 
         <strong> discounted admission </strong> at events with fees, and the 
         <strong> ability to vote </strong> for General Board elections.
         <span> </span><u className={styles.underline}>You don’t have to be a member to come to our events!</u><span> </span>
-         Once you step foot into our space, you are already part of our community😎.</p>
+        Once you step foot into our space, you are already part of our community😎.
+        
+        <br />
+        <br />Links:
+        <br />Video
+        <br />Quiz 1 (not active)
+        <br />Quiz 2
+        <br />*Please note, the Quiz #1 is not active. You currently DO NOT need to submit this to achieve membership. Please fulfill the other steps
+        <br />
+        <br />
+        </p>
       </div>
       <Image className={styles.image}
         src='/membership.svg'
