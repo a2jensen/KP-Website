f18c8a55d235f9226b8fb27a24c19df7a035800a
Angelo Jensen <a2jensen@ucsd.edu>
2024-09-19 21:12:20 -0700

    added the kp favicon logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico"/>
+      </head>
       <body className={outfit.className}>
         <Nav />
         {children}
