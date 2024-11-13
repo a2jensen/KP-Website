d0d415b17ef6ba04112ce59e9564b9b1de7a6dee
nicole-beatrice-go <nicolebeatricego@gmail.com>
2024-11-12 23:16:40 -0800

    fixed responsiveness bug, added backgrounds svgs

--- components/aboutPage/who/who.jsx
+++ components/aboutPage/who/who.jsx
@@ -11,10 +11,6 @@ export default function who() {
   return (
     <div className={styles.group}>  
       {/* include stickers within .group nest */}
-      <Image src={"/star.svg"} alt={"star sticker"} width={0} height={0} className={styles.star1}/>
-      <Image src={"/star.svg"} alt={"star sticker"} width={0} height={0} className={styles.star2}/>
-      <Image src={"/star.svg"} alt={"star sticker"} width={0} height={0} className={styles.star3}/>
-      <Image src={"/star.svg"} alt={"star sticker"} width={0} height={0} className={styles.star4}/>
       <Image src={"/world1.svg"} alt={"world sticker"} width={0} height={0} className={styles.world1}/>
       <Image src={"/jeepney1.svg"} alt={"jeep sticker"} width={0} height={0} className={styles.jeep1}/>
 
